e1eaceacc94bbd6ff8a437d890688c4486a77d31
machou <fmachou@free-mobile.fr>
2024-12-06 18:39:30 +0100

    ajout test invalid json

---
+++ tests/invalid_json.py
@@ -0,0 +1,20 @@
+import pytest
+import json
+
+# Test des données manquantes
+@pytest.fixture
+def client():
+    from api.app import app  # Importez votre application Flask
+    with app.test_client() as client:
+        yield client
+
+
+def test_invalid_json(client):
+    # Test si l'API retourne une erreur si le JSON est invalide
+    response = client.post('/predict', data="Invalid data")
+    assert response.status_code == 400
+    response_data = json.loads(response.data)
+    assert 'error' in response_data
+    assert response_data['error'] == "Le format des données est invalide, JSON attendu."
+
+
